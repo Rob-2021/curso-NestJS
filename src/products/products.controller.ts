10dfa03c15e08efdd013edfdb13c156111e2aef5
Rob-2021 <dragrober.rar@gmail.com>
2024-11-13 09:05:29 -0400

    CRUD

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
+
+    constructor(private productsService:ProductsService){
+
+    }
+
+    //ruta principal encuentra todo
     @Get()
-    getProduct(): string {
-        return 'retorna todos los productos';
+    findAll(){
+        return this.productsService.findAll();
+    }
+
+    // /products/:id
+    //retorna un producto
+    @Get(':id')
+    findOne(@Param('id') id:string) {
+        return this.productsService.findOne(Number(id));
+    }
+
+    //ruta para crear
+    @Post()
+    create(@Body() payload: any) {
+        return this.productsService.create(payload);
+    }
+
+    //actualizar
+    @Put(':id')
+    update(@Param('id') id:number, @Body() payload:any){
+        return this.productsService.update(id, payload);
+    }
+
+    //eliminar
+    @Delete(':id')
+    delete(@Param('id') id:number){
+        return this.productsService.delete(id);
     }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,86 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from './entities/product.entity';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+
+    private counterId = 1;
+    private products:Product[] = [
+        {
+            id: 1,
+            name: 'Producto 1',
+            description: 'Esto es un producto 1',
+            price: 1000,
+            stock: 2
+        }
+    ]
+
+    //metodo para encontrar todos los productos
+    findAll(){
+        return this.products;
+    }
+
+    //metodo para encontrar un producto
+    findOne(id:number){
+        return this.products.find((item) => item.id === id);
+    }
+
+    //metodo para crear un producto
+    create(payload:any){
+        this.counterId = this.counterId + 1;
+
+        const newProduct = {
+            id: this.counterId,
+            ...payload
+        };
+        this.products.push(newProduct);
+        return newProduct;
+    }
+
+    // //metodo para actualizar un producto
+    // update(id:number, payload:any){
+    //     const index = this.products.findIndex((item) => item.id === id);
+    //     if(index === -1){
+    //         return null;
+    //     }
+    //     this.products[index] = {
+    //         ...this.products[index],
+    //         ...payload
+    //     };
+    //     return this.products[index];
+    // }
+
+    // //metodo para eliminar un producto
+    // delete(id:number){
+    //     const index = this.products.findIndex((item) => item.id === id);
+    //     if(index === -1){
+    //         return null;
+    //     }
+    //     const product = this.products[index];
+    //     this.products = this.products.filter((item) => item.id !== id);
+    //     return product;
+    // }
+
+    update(id: number, payload: any) {
+        const index = this.products.findIndex((item) => item.id === id);
+        if (index === -1) {
+            throw new Error(`Product con id ${id} no encontrado`);
+        }
+        this.products[index] = {
+            ...this.products[index],
+            ...payload,
+        };
+        return this.products[index];
+    }
+    
+    // Método para eliminar un producto
+    delete(id: number) {
+        const index = this.products.findIndex((item) => item.id === id);
+        if (index === -1) {
+            throw new Error(`Product con id ${id} no encontrado`);
+        }
+        const product = this.products[index];
+        this.products = this.products.filter((item) => item.id !== id);
+        return product;
+    }
+}
